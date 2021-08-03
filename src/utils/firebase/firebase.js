7cbe127b5ba5ca638504b21227789766c1eed500
ateebasif902 <ateeb.asif902@gmail.com>
2021-08-03 12:53:21 +0500

    environmental variables added for firebase configs

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -3,17 +3,23 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 import "firebase/storage";
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTzig7iRcyjqkNpF4ihuD4igCxrOQhXPA",
-  authDomain: "fan-crowd-81a6a.firebaseapp.com",
-  projectId: "fan-crowd-81a6a",
-  storageBucket: "fan-crowd-81a6a.appspot.com",
-  messagingSenderId: "391936695307",
-  appId: "1:391936695307:web:f6bbe938df909730513b0e",
+  // apiKey: "AIzaSyDTzig7iRcyjqkNpF4ihuD4igCxrOQhXPA",
+  // authDomain: "fan-crowd-81a6a.firebaseapp.com",
+  // projectId: "fan-crowd-81a6a",
+  // storageBucket: "fan-crowd-81a6a.appspot.com",
+  // messagingSenderId: "391936695307",
+  // appId: "1:391936695307:web:f6bbe938df909730513b0e",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
-
-// firebase.initializeApp(firebaseConfig);
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
